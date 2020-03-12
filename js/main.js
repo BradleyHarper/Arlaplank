fa2a3d229b250699a5d03dd8743502e890f4ba1f
Matt Thomas <mthomas@neces.com>
2020-03-12 15:58:13 -0500

    Commit JS and remove node_modules

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-// alert('Hey nurd, this site is in BETA form');
+class Book {
+    title;
+    author;
+    description;
+    coverImageUrl;
+
+    constructor(title, author, description = null, coverImageUrl = null) {
+        // Required parameters
+        this.title = title;
+        this.author = author;
+
+        // Optional parameters
+        this.description = description;
+        this.coverImageUrl = coverImageUrl;
+    }
+}
+
+const books = [];
+
+function setupBooks() {
+    const youDontKnowJS = new Book(
+        'You don\'t know JS',
+        'Kyle Simpson',
+        'This is a series of books diving deep into the core mechanisms of the JavaScript language.',
+        './img/you-dont-know-js.webp'
+    );
+
+    books.push(youDontKnowJS);
+}
+
+function render() {
+    setupBooks();
+
+    const parentElement = document.getElementById('main-container');
+    const template = document.getElementById('book-template');
+
+    if(books.length > 0) {
+        books.forEach(book => {
+            const bookElement = template.cloneNode(true);
+            parentElement.appendChild(bookElement);
+        });
+    }
+}
+
+render();
